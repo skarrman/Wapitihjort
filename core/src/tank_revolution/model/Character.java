77d906c81c768c46cdeaae4bba199592211013fc
antonhager <anthage@student.chalmers.se>
2017-03-30 15:41:01 +0200

    Created character, player and npc. Created GameSession and renamed GameModel.

---
+++ core/src/tank_revolution/model/Character.java
@@ -0,0 +1,7 @@
+package tank_revolution.model;
+
+/**
+ * Created by antonhagermalm on 2017-03-30.
+ */
+public interface Character {
+}
---
+++ core/src/tank_revolution/model/GameSession.java
@@ -0,0 +1,8 @@
+package tank_revolution.model;
+
+/**
+ * Created by antonhagermalm on 2017-03-30.
+ */
+public class Game {
+
+}
