28b79e243e916a9bc538d9228486f13e4e8054ee
skarrman <simonkarrman@icloud.com>
2017-03-30 10:32:43 +0200

    Setting up project

---
+++ desktop/src/tank_revolution/controller/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package tank_revolution.controller.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import tank_revolution.controller.TankRevolutionController;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TankRevolutionController(), config);
+	}
+}
