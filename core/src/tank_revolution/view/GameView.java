2093c2c4469a48436b15295db375a6bf4ad8ff9b
skarrman <simonkarrman@icloud.com>
2017-03-31 14:23:38 +0200

    Added tank sprite sheets

--- core/src/tank_revolution/view/GameView.java
+++ core/src/tank_revolution/view/GameView.java
@@ -2,10 +2,13 @@ package tank_revolution.view;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.*;
+import com.badlogic.gdx.math.Vector2;
+import tank_revolution.model.Character;
 import tank_revolution.model.GameModel;
+
+import javax.swing.text.Position;
+import java.util.List;
 
 import static com.badlogic.gdx.graphics.GL20.GL_COLOR_BUFFER_BIT;
 
@@ -14,20 +17,25 @@ import static com.badlogic.gdx.graphics.GL20.GL_COLOR_BUFFER_BIT;
  */
 public class GameView {
     private Batch batch;
-    private Sprite sprite;
+    private TextureAtlas textureAtlas;
     private GameModel model;
+    private List<Character> characterList;
+
 
     public GameView(GameModel model){
         this.model = model;
         batch = new SpriteBatch();
-        sprite = new Sprite(new Texture(Gdx.files.internal("badlogic.jpg")));
+        characterList = model.getCharacterList();
+        textureAtlas = new TextureAtlas(Gdx.files.internal("GreenTank.txt"));
+
     }
 
     public void update(){
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL_COLOR_BUFFER_BIT);
         batch.begin();
-        sprite.draw(batch);
+        Vector2 pos = characterList.get(0).getTank().getBody().getPosition();
+        batch.draw(textureAtlas.getRegions().first(),pos.x,pos.y);
         batch.end();
     }
 
