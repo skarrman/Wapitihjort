77ad1fe6dfd84ae66b4334eb8a5a1039687658c4
skarrman <simonkarrman@icloud.com>
2017-04-04 13:45:41 +0200

    Changed the display size of the desktop view

--- desktop/src/tank_revolution/controller/desktop/DesktopLauncher.java
+++ desktop/src/tank_revolution/controller/desktop/DesktopLauncher.java
@@ -7,8 +7,8 @@ import tank_revolution.controller.TankRevolutionController;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1920;
-		config.width = 1080;
+		config.width = 960;
+		config.height = 540;
 		new LwjglApplication(new TankRevolutionController(), config);
 	}
 }
