c362634e36dc466fe65b3b4cf92f65954108ea0d
skarrman <simonkarrman@icloud.com>
2017-04-04 09:36:14 +0200

    Added a debug renderer to the view

--- core/src/tank_revolution/view/GameView.java
+++ core/src/tank_revolution/view/GameView.java
@@ -1,9 +1,15 @@
 package tank_revolution.view;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.*;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.utils.Array;
 import tank_revolution.model.Character;
 import tank_revolution.model.GameSession;
 import java.util.ArrayList;
@@ -33,6 +39,15 @@ public class GameView {
     /** The graphical representation of the flying projectile */
     private Sprite projectile;
 
+    /** An orthoginal camera */
+    private OrthographicCamera camera;
+
+    /** A debug renderer to debug the box2d bodies */
+    private Box2DDebugRenderer debugRenderer;
+
+    /** A matrix to help the debug process */
+    private Matrix4 debugMatrix;
+
     /**
      * The standard constructor that initialize everything to make the graphics work.
      * @param session The current game session.
@@ -45,7 +60,10 @@ public class GameView {
         textureAtlases.add(new TextureAtlas(Gdx.files.internal("GreenTank.txt")));
         textureAtlases.add(new TextureAtlas(Gdx.files.internal("WhiteTank.txt")));
         projectile = new Sprite(new Texture(Gdx.files.internal("Projectile.png")));
-        metersToPixels = Gdx.graphics.getWidth()/50; //Calculates the ratio between the pixels of the display to meters in the world.
+        metersToPixels = Gdx.graphics.getWidth()/50f; //Calculates the ratio between the pixels of the display to meters in the world.
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        debugMatrix = new Matrix4(camera.combined);
+        debugRenderer = new Box2DDebugRenderer();
 
     }
 
@@ -54,9 +72,16 @@ public class GameView {
      * It draws out all the graphical representation of the objects in the world.
      */
     public void update(){
+        camera.update();
+
         Gdx.gl.glClearColor(0.980392f, 0.980392f, 0.823529f, 1);
         Gdx.gl.glClear(GL_COLOR_BUFFER_BIT);
+
+        batch.setProjectionMatrix(camera.combined);
+        debugMatrix = batch.getProjectionMatrix().cpy().scale(metersToPixels, metersToPixels, 0);
+        camera.position.set(new Vector3(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2, 0));
         batch.begin();
+
         if(session.isProjectileFlying()){
             Vector2 projectilePos = session.getProjectilePosision();
             projectile.setPosition(projectilePos.x * metersToPixels - projectile.getWidth()/2, projectilePos.y * metersToPixels - projectile.getHeight()/2);
@@ -67,9 +92,10 @@ public class GameView {
             atlasRegion = textureAtlases.get(i).getRegions().first();
             Vector2 pos = characterList.get(i).getTank().getBody().getPosition();
             batch.draw(atlasRegion, (pos.x * metersToPixels) - atlasRegion.getRegionWidth()/2,
-                                    (pos.y * metersToPixels) - atlasRegion.getRegionHeight()/2);
+                                    (pos.y * metersToPixels) - atlasRegion.getRegionHeight()/4);
         }
         batch.end();
+        debugRenderer.render(characterList.get(0).getTank().getBody().getWorld(), debugMatrix);
     }
 
     /**
