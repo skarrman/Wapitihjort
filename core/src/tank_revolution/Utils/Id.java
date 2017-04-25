21dbc754fc50d7bde5a2d4b675f01dccd1b3bd17
antonhager <anthage@student.chalmers.se>
2017-04-25 08:21:07 +0200

    - Removed tank in the enums

--- core/src/tank_revolution/Utils/Id.java
+++ core/src/tank_revolution/Utils/Id.java
@@ -4,9 +4,9 @@ package tank_revolution.Utils;
  * Created by antonhagermalm on 2017-04-25.
  */
 public enum Id {
-        EXPLOSION,
-        PLAYER1_TANK,
-        PLAYER2_TANK,
-        PLAYER3_TANK,
-        PLAYER4_TANK,
+    EXPLOSION,
+    PLAYER1,
+    PLAYER2,
+    PLAYER3,
+    PLAYER4,
 }
