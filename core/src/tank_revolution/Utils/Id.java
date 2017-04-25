7baabefe91d8930202ea2ed1d2d5eb802ef2873f
antonhager <anthage@student.chalmers.se>
2017-04-25 08:16:19 +0200

    - Added the id enum to replace Strings in identifiers for players and textureAtlases

---
+++ core/src/tank_revolution/Utils/Id.java
@@ -0,0 +1,12 @@
+package tank_revolution.Utils;
+
+/**
+ * Created by antonhagermalm on 2017-04-25.
+ */
+public enum Id {
+        EXPLOSION,
+        PLAYER1_TANK,
+        PLAYER2_TANK,
+        PLAYER3_TANK,
+        PLAYER4_TANK,
+}
