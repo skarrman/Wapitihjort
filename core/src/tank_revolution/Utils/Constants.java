ce9e7e1fb2dbad9e35eb9959b5e98d9e8479e36b
antonhager <anthage@student.chalmers.se>
2017-04-27 11:14:40 +0200

    - Added Utils.Constants - Moved the mapWidth to Utils.Constants

---
+++ core/src/tank_revolution/Utils/Constants.java
@@ -0,0 +1,13 @@
+package tank_revolution.Utils;
+
+/**
+ * Created by antonhagermalm on 2017-04-27.
+ */
+public class Constants {
+    private static final float mapWidth = 50f;
+
+    public static float getMapWidth() {
+        return mapWidth;
+    }
+
+}
