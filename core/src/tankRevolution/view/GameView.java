b226c11c5b0e7a11b5886fb6efe64917179f9f37
JakobErlandsson <jakobedvarderlandsson@gmail.com>
2017-05-11 14:10:14 +0200

    Refactoring

--- core/src/tankRevolution/view/GameView.java
+++ core/src/tankRevolution/view/GameView.java
@@ -38,7 +38,7 @@ public class GameView implements Viewable {
      * True = debug: ON
      * False = debug: OFF
      */
-    private boolean deBugMode = true;
+    private boolean deBugMode = false;
 
     /**
      * The graphical batch that draws on the screen
@@ -277,7 +277,7 @@ public class GameView implements Viewable {
         for(float[] v : vertices){
             for(int i = 0; i < v.length; i++){
                 //System.out.println("Vertex value: "+v[i]);
-                //v[i] = v[i] * Constants.metersPerPixel();
+                v[i] = v[i] * Constants.pixelsPerMeter();
             }
             shapeRenderer.begin(ShapeType.Line);
             shapeRenderer.setColor(1,1,0,1);
