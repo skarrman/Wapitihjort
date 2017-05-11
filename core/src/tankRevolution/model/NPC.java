78dbef9f2b344c8c70203851824a6f7d0d1f581a
Jakob Wall <jakob.wall@outlook.com>
2017-05-11 10:38:33 +0200

    Implemented the different difficultylevels for the NPC

--- core/src/tankRevolution/model/NPC.java
+++ core/src/tankRevolution/model/NPC.java
@@ -13,15 +13,18 @@ import java.util.Random;
 public class NPC extends Character{
 
     private NPCDifficulty difficulty;
+    private Random rand;
 
     NPC(Id id, NPCDifficulty difficulty) {
         super(id, true);
         this.difficulty = difficulty;
+        rand = new Random();
     }
 
     NPC(NPC npc){
         super(npc);
         this.difficulty = npc.difficulty;
+        rand = new Random();
     }
 
     public Vector getShootVector(List<Tank> tanks, List<Point> positions){
@@ -29,7 +32,20 @@ public class NPC extends Character{
         Point ownPoint = positions.get(ownTankPlace);
         int opponentPlace = getRandomPlace(ownTankPlace, tanks.size());
         Point opponentPoint = positions.get(opponentPlace);
-        Vector vector = calculateIdealVector(ownPoint, opponentPoint);
+        Vector vector;
+        float random = (float) Math.random();
+        if (random < this.difficulty.getBadLimit()){
+            vector = getBadVector(ownPoint, opponentPoint);
+        }else if(random < this.difficulty.getGoodLimit()){
+            vector = getNormalVector(ownPoint, opponentPoint);
+        }else if(random < this.difficulty.getPerfectLimit()){
+            vector = getGoodVector(ownPoint, opponentPoint);
+        }else if(random <= 1){
+            vector = getPerfectVector(ownPoint, opponentPoint);
+        }else{
+            System.out.println("No Difficulty set for NPC.");
+            vector = calculateIdealVector(ownPoint, opponentPoint);
+        }
         return vector;
     }
 
@@ -64,6 +80,33 @@ public class NPC extends Character{
         float timeInAir = (float)((VelocityY/-Constants.getGravity())+(Math.sqrt((VelocityY/Constants.getGravity())*(VelocityY/Constants.getGravity())
                 -(2*deltaY)/(-Constants.getGravity()))));
         return new Vector(deltaX/timeInAir, VelocityY);
+    }
+
+    private Vector getBadVector(Point own, Point opponent){
+        float diff = 20 + rand.nextInt(30);
+        return getVectorWithDiff(own, opponent, diff);
+    }
+    private Vector getNormalVector(Point own, Point opponent){
+        float diff = 10 + rand.nextInt(10);
+        return getVectorWithDiff(own, opponent, diff);
+    }
+    private Vector getGoodVector(Point own, Point opponent){
+        float diff = 5 + rand.nextInt(2);
+        return getVectorWithDiff(own, opponent, diff);
+    }
+    private Vector getPerfectVector(Point own, Point opponent){
+        float diff = 0;
+        return getVectorWithDiff(own, opponent, diff);
+    }
+
+    private Vector getVectorWithDiff(Point own, Point opponent, float diff){
+        Random rand = new Random();
+        if(rand.nextBoolean())
+            opponent = new Point(opponent.getX()+diff, opponent.getY());
+        else
+            opponent = new Point(opponent.getX()-diff, opponent.getY());
+
+        return calculateIdealVector(own, opponent);
     }
 
     public void setNewTurn(){
